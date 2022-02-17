74bc5d0103101de6a4eef364af666960e7bf7b61
josiemateo99 <josiemateo99@gmail.com>
2022-02-17 00:03:01 -0500

    Login UI Connections

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Josephine Mateo on 2/17/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
