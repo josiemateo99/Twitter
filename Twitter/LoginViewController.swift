2c5f6c3faddcbaa2c026cbe98108a470456064ff
josiemateo99 <josiemateo99@gmail.com>
2022-02-17 00:19:55 -0500

    Login UI + Login Compelete

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,7 +16,17 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        let myURL = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myURL, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { Error in
+            print("Could not log in!")
+        })
+    }
+    
     /*
     // MARK: - Navigation
 
